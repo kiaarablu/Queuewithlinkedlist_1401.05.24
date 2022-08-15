d77c2f687b12c18dc2b693583baf8ba7904ceac0
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-15 14:10:27 +0430

    Add files via upload

---
+++ QueueWithLinkedlist_1401.05.24/QueueWithLinkedlist_1401.05.24/QueueWithLinkedlist.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueWithLinkedlist_1401._05._24
+{
+    internal class QueueWithLinkedlist
+    {
+
+
+    
+        public class Node
+        {
+            public int Value;
+            public Node Next;
+        }
+
+        public class QueueImplementLinkedList
+        {
+            Node head;
+            Node rear;
+
+            bool IsEmpty()
+            {
+                if (head == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void DeQueue()
+            {
+                if (IsEmpty())
+                {
+                    Console.WriteLine("Queue Is Empty!!!");
+                }
+                else
+                {
+                    head = head.Next;
+                }
+            }
+
+            public void EnQueue(int val)
+            {
+                if (IsEmpty())
+                {
+                    Node newnode = new Node();
+                    newnode.Value = val;
+                    head = newnode;
+                    head.Next = null;
+                    rear = head;
+                }
+                else
+                {
+                    Node newNode = new Node();
+                    newNode.Value = val;
+                    rear.Next = newNode;
+                    rear = newNode;
+                }
+            }
+
+            public void Print()
+            {
+                if (IsEmpty())
+                {
+                    Console.WriteLine("Queue Is Empty!!!");
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp != rear.Next)
+                    {
+                        Console.WriteLine(temp.Value);
+                        temp = temp.Next;
+                    }
+                }
+            }
+        }
+    }
+}
+
